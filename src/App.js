7d52ec2473b71c34866fca4cc5559fc58f396667
Samuel <samlee1097@yahoo.com>
2022-01-14 19:59:38 -0500

    create client and server js files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 
 const username = prompt("What is your username?");
 
+// Open socket.io server to localhost:3000
 const socket = io("http://localhost:3000",{
   // Need to specify transports in order to bypass waiting messages (buffers)
   transports: ["websocket", "polling"]
@@ -19,16 +20,32 @@ function App() {
   useEffect(()=> {
       socket.on("connect", () => {
         socket.emit("username", username)
-      })
+      });
 
       socket.on("users", users => {
         setUsers(users)
-      })
+      });
 
       socket.on("message", message => {
         setMessages(messages => [...messages, message])
+      });
+
+      socket.on("connected", user => {
+        setUsers(users => [...users, user]);
       })
-  }, [])
+
+      socket.on("disconnected", id => {
+        setUsers(users => {
+            users.filter(user => user.id !== id);
+        });
+      });
+  }, []);
+
+  function handleSubmit(e){
+    e.preventDefault();
+    socket.emit("send", message);
+    setMessage("")
+  }
 
   return (
     <div>
--- src/server.js
+++ src/server.js
@@ -1,3 +1,45 @@
-// Create an http server using createServer()
+// Create an http server using createServer
 const server = require("http").createServer();
-const io
+// Wrap server using socket.io w/ transports
+const io = require("socket.io")(server, {
+    transports: ["websockets", "polling"]
+});
+
+// Empty object to store users
+const users = {};
+
+// Run callback anytime a user connects to the socket server
+io.on("connection", client => {
+    // Event to store new user info once username is submitted
+    client.on("username", username => {
+        const user = {
+            name: username,
+            id: client.id
+        };
+
+        // Stores new user into users object with client.id as key
+        users[client.id] = user;
+        // Broadcast an event to all the users once the user is created
+        io.emit("connected", user);
+        // Emit the users object to others users to see all users connected
+        io.emit("users", Object.values(users))
+    });
+
+    // Event whenever a message is sent to the server
+    client.on("send", message => {
+        io.emit("message", {
+            text: message,
+            date: new Date().toISOString(),
+            user: user[client.id]
+        });
+    });
+
+    // Event whenever a user leaves the server
+    client.on("disconnect", () => {
+        const username = users[client.id];
+        delete users[client.id];
+        io.emit("disconnected", client.id)
+    });
+});
+// Start & host a server on port:3000
+server.listen(3000)
